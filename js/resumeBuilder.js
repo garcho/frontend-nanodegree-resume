2e50fd56b8ed30047f308aeb4275e51d9cb7a289
Luis E. Garcia <garcia@lacasonadelpatio.net>
2015-01-28 15:57:52 -0500

    Archivos hasta clase 16 de lección 1.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,42 @@
+
+var name = "Luis García"; 
+var role = "Director General"; 
+var skills = ["x", "y", "z"]; 
+
+
+
+var formattedName = HTMLheaderName.replace("%data%", name); 
+
+var formattedRolle = HTMLheaderRole.replace("%data%", role); 
+
+$("#header").prepend(formattedRolle);
+$("#header").prepend(formattedName);
+
+
+var bio = {
+			"name" : name, 
+			"role" : role, 
+			"contacts" : {"tel" : "+57.1.21238805", "email" : "garcia@lacasonadelpatio.net"}, 
+			"pictureURL" : "https://cdn4.iconfinder.com/data/icons/avatar-and-user/78/Avatar_person_user_character_man_woman_human-05-128.png",
+			"welcomeMessage" : "Hello", 
+			"skills" : skills
+
+	}	
+
+
+
+	$("#header").append(bio.name);
+	bio.city = "Bogota"; 
+
+	/*
+	
+	
+	var awesomeThoughts = "I am Luis García and I am awesome."; 
+	
+	var fun = awesomeThoughts.replace("awesome", "FUN"); 
+	
+	console.log(awesomeThoughts);
+	console.log(fun);
+	
+	$("#main").append(fun); 
+*/
